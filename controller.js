47acbaf8b3795284bdf0bf7ec9dd48a4eff19fab
junior <antelo.juan@gmail.com>
2015-09-01 12:37:57 -0400

    setup code structure

---
+++ controller.js
@@ -0,0 +1,8 @@
+angular.module('roomateApplicantTracker', [])
+
+.controller('TableController', function() {
+    var tableController = this;
+    tableController.addRow = function() {}
+    tableController.deleteRow = function() {}
+    tableController.updateRow = function() {};
+})
